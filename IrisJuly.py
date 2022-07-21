d177c95e5842fc0687a8520a0bcc636cb232c86f
zarrif nordin <109710271+zarrifnordin@users.noreply.github.com>
2022-07-21 17:15:49 +0800

    Update IrisJuly.py

--- IrisJuly.py
+++ IrisJuly.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
+
 from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
@@ -27,9 +27,9 @@ df = user_input_features()
 st.subheader('User Input parameters')
 st.write(df)
 
-iris = datasets.load_iris()
-X = iris.data
-Y = iris.target
+iris = pd.read_csv('https://raw.githubusercontent.com/zarrifnordin/AAA-IrisProject/main/IRIS.csv')
+X = iris.drop('species', axis = 1)
+Y = iris('species')
 
 clf = RandomForestClassifier()
 clf.fit(X, Y)
@@ -38,11 +38,11 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-st.write(iris.target_names)
+dataf= pd.DataFrame(['Iris-setosa','Iris-versicolor','Iris-virginica'])
+st.write(dataf)
 
 st.subheader('Prediction')
-st.write(iris.target_names[prediction])
-#st.write(prediction)
+
 
 st.subheader('Prediction Probability')
 st.write(prediction_proba)
